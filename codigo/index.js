4cbf234876448bd7b76ec4d9cf4c084fc4dd7406
josuemishqui <74439079+josuemishqui@users.noreply.github.com>
2021-02-26 20:11:00 -0500

    Update index.js

--- codigo/index.js
+++ codigo/index.js
@@ -15,7 +15,9 @@ Vamos a inicializar la conexion a la  base de datos
 const app = express();
 require('./lib/passport');
 
-//Configuraciones
+/*Configuraciones
+Colocamos las configuraciones que necesita el servidor.
+*/
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
@@ -27,7 +29,10 @@ app.engine('.hbs', exphbs({
 }))
 app.set('view engine', '.hbs');
 
-//Middlewares
+/*Middlewares
+Son funciones que se ejecutan cada vez que un usuario envia una peticion o que cada vez una aplicacion cliente 
+envia una peticion al servidor.
+*/
 app.use(session({
     secret: 'mysqlnodesession',
     resave: false,
@@ -41,7 +46,9 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Variables globales
+/*Variables globales
+Se pueden colocar variables que toda la aplicacion necesite
+*/
 app.use((req, res, next) => {
     app.locals.success = req.flash('success');
     app.locals.success = req.flash('message');
@@ -49,16 +56,23 @@ app.use((req, res, next) => {
     next();
 });
 
-//Rutas
+/*Rutas
+Es muy importante porque aqui definimos las URL de nuestro servidor,
+osea lo que haran cuando el usuario visite la ruta.
+*/
 app.use(require('./routes'));
 app.use(require('./routes/authentication'));
 app.use('/tasks', require('./routes/tasks'));
 app.use('/auth', require('./routes/authentication'));
 
-//Public
+/*Public
+Colocamos una carpeta donde vamos a colocar todo el codigo que el navegador pueda acceder.
+*/
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Iniciar el servidor
+/*Iniciar el servidor
+Utilizamos esto para empezar a usar el servidor.
+*/
 app.listen(app.get('port'),() => {
     console.log('Servidor en el puerto', app.get('port'));
 });
