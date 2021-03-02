1ab105b0a42770eb251e71fccd34edfe9bcb0cdf
josuemishqui <74439079+josuemishqui@users.noreply.github.com>
2021-03-01 23:08:40 -0500

    Update handlebars.js

--- codigo/lib/handlebars.js
+++ codigo/lib/handlebars.js
@@ -1,9 +1,9 @@
-const { format } = require('timeago.js');
-const helpers = {};
+const { format } = require('timeago.js');   //requerimos timeago, lo ejecutamos y se vuelve una instancia.
+const helpers = {};                        //herlpers ens un objeto que sera utilizado por la vista de handlebars.
 
-helpers.timeago = (timestamp) => {
-    console.log(format(timestamp));
-    return format(timestamp);
+helpers.timeago = (timestamp) => {     //timestamp es utilizado desde la vista.
+    console.log(format(timestamp));    //Estamos importando timeago
+    return format(timestamp);         //Demostrara cuanto tiempo a pasado desde la publicacion.
 };
 
 helpers.ifSmaller = (arg1, arg2, options) => {
