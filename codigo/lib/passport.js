35879366283919f2eabf530622da9431cc72cbda
josuemishqui <74439079+josuemishqui@users.noreply.github.com>
2021-03-02 16:59:22 -0500

    Update passport.js

--- codigo/lib/passport.js
+++ codigo/lib/passport.js
@@ -1,14 +1,14 @@
-const passport = require('passport'); 
-const LocalStrategy = require('passport-local').Strategy; 
+const passport = require('passport');     //Definimos el metodo el tipo de autoticacion con la base de datos.
+const LocalStrategy = require('passport-local').Strategy;   //Vamos a guardar con nuestra base de datos
 
 const pool = require('../database');
 const helpers = require('../lib/helpers');
 
-passport.use('local.signin', new LocalStrategy({
-    usernameField: 'cedula',
-    passwordField: 'password',
-    passReqToCallback: true
-}, async (req, cedula, password, done) => {
+passport.use('local.signin', new LocalStrategy({     //Vamos a poder definir nuestra autoticacion
+    usernameField: 'cedula',  // Lo estamos recibiendo este campo atravez de usernamefiel.
+    passwordField: 'password',     //Es para decir atravez de que campo voy a recibir mi contraseña.
+    passReqToCallback: true      //Es para poder recibir el objeto req dentro de la funcion que ejecuta este local strategy
+}, async (req, cedula, password, done) => {   //metodo asincronos
     const rows = await pool.query('SELECT * FROM usuarios WHERE cedula = ?', cedula);
     if (rows.length > 0){
         const usuario = rows[0];
