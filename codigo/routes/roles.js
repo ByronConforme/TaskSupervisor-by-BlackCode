513e35a3d0056bd3e3ebfe3620f702661b9f50b3
Byron Conforme <andres99bcd@hotmail.com>
2021-03-19 16:15:02 -0500

    Protegida la ruta de roles

--- codigo/routes/roles.js
+++ codigo/routes/roles.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const pool = require('../database');
 
-const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
+const { isLoggedIn, hasPermission } = require('../lib/auth');
 
-router.get('/add', isLoggedIn, (req, res)=>{
+router.get('/add', isLoggedIn, hasPermission, (req, res)=>{
     res.render('roles/add');
 });
 
-router.post('/add', isLoggedIn, async (req, res)=>{
+router.post('/add', isLoggedIn, hasPermission, async (req, res)=>{
     const { nombre, nivel } = req.body;
     const nuevoRol = {
         nombre,
@@ -21,26 +21,26 @@ router.post('/add', isLoggedIn, async (req, res)=>{
     res.redirect('/roles');
 });
 
-router.get('/', isLoggedIn, async (req, res) => {
+router.get('/', isLoggedIn, hasPermission, async (req, res) => {
     const roles = await pool.query('SELECT * FROM roles WHERE estado = "A"');
     res.render('roles/list', {roles});
 });
 
-router.get('/delete/:id', isLoggedIn, async (req, res) => {
+router.get('/delete/:id', isLoggedIn, hasPermission, async (req, res) => {
     const { id } =req.params;
     await pool.query("UPDATE roles SET estado = 'I' WHERE id = ?", [id]);
     req.flash('success', 'Rol eliminado exitosamente.');
     res.redirect('/roles');
 });
 
-router.get('/edit/:id', isLoggedIn, async (req, res) => {
+router.get('/edit/:id', isLoggedIn, hasPermission, async (req, res) => {
     const { id } =req.params;
     const roles = await pool.query("SELECT * FROM roles WHERE id = ?", [id]);
     console.log(roles);
     res.render('roles/edit', {rol: roles[0]});
 });
 
-router.post('/edit/:id', isLoggedIn, async (req, res)=>{
+router.post('/edit/:id', isLoggedIn, hasPermission, async (req, res)=>{
     const { id } =req.params;
     const { nombre, nivel } = req.body;
     const nuevoRol = {
