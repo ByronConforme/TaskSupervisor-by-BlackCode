54d271ea2cae3fd5026fd32a93cf71fe4c661edb
WALTER <wisbes1996@hotmail.com>
2021-03-18 21:52:03 -0500

    Update keys.js

--- codigo/keys.js
+++ codigo/keys.js
@@ -2,7 +2,7 @@ module.exports = {                //Sirve para exportar un objeto
     database:{
         host: 'localhost',
         user: 'root',                              //Con estos comandos se deberia conectar a nuestra base de datos
-        port: '3306',
+        port: '3308',
         password: '',
         database: 'db_tasksupervisor'
     }
